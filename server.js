13a42ab9b81d2bdf6aa18ac53534e346721aa165
Jared Thomas <jjthom87@yahoo.com>
2016-08-14 21:45:30 -0400

    here

---
+++ server.js
@@ -0,0 +1,35 @@
+//NPM install packages...Dependencies
+var express = require ('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+// var ParseServer = require('parse-server').ParseServer;
+
+//Sets up the the express app
+var app = express();
+
+//Where all of the post information is being pushed to
+//setting up the port that the server will be listening on
+var PORT = process.env.PORT || 5000;
+
+//Sets up the express app to handle parsing
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({ 
+	limit: '50mb',
+	extended: true, 
+	parameterLimit:50000}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json'}));
+
+//displaying data from the home.html file
+app.get('/', function(reckor,resin){
+	resin.sendfile(path.join(process.env.PWD, 'app/products/html/index.html'));
+});
+
+// app.use('/parse', api);
+//lets the server recognize the js files
+app.use('/static', express.static('app/products'));
+
+//starts the server with the listening queue
+app.listen(PORT, function(){
+	console.log("Listening on port", PORT);
+});
